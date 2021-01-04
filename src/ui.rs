23a9e99bdc030745c513efe888db71bed65c14f2
Eino Korte <e.i.korte@gmail.com>
2021-01-04 22:21:02 +0200

    added scenarios

--- src/ui.rs
+++ src/ui.rs
@@ -1,4 +1,9 @@
-const empty_texts: &[&str] = &["You hear stones crumbling..."];
+use rand::Rng;
+
+
+
+const empty_texts: &[&str] = &["You hear stones crumbling...", "The earth moves...", "You listen to beetles..."];
+const scenarios: &[&str] = &["wounded_man", "empty_cavern", "drill_wreck", "ancient_ruins", "ancient_remains"];
 
 
 pub fn draw_ui(state: &str) {
@@ -46,14 +51,99 @@ pub fn draw_descend(map: &mut crate::world::WorldMap) -> &str{
 	    map.ship_y += 1;
 	}
 
-	//refresh map
 	
 	if map.content[map.ship_y as usize][map.ship_x as usize] == 2 {
-	   
+
+	    let mut rng = rand::thread_rng();
 	    
+	    if scenarios[rng.gen_range(0..scenarios.len())] == "wounded_man"{
+		println!("You tear open a small cave where lies a man, clearly wounded.
+1. Leave him be and keep drilling
+2. Try to save him");
+		
+		let mut line2 = String::new();
+		let input2 = std::io::stdin().read_line(&mut line).unwrap();
+		if line2.trim() == "1" {
+		    return "";
+		}
+		
+		if line2.trim() == "2" {
+		    return "";
+		}
+	    }
+	    
+	    else if scenarios[rng.gen_range(0..scenarios.len())] == "empty_cavern"{
+		
+		println!("You face a dark and empty cavern.
+1. Keep drilling
+2. Enter the cave");
+		
+		let mut line2 = String::new();
+		let input2 = std::io::stdin().read_line(&mut line).unwrap();
+		if line2.trim() == "1" {
+		    return "";
+		}
+		
+		if line2.trim() == "2" {
+		    return "";
+		}
+	    }
+	    
+	    else if scenarios[rng.gen_range(0..scenarios.len())] == "drill_wreck"{
+		
+		println!("A wreck of a transport drill lies abandoned before you. Nothing has been here for a long time.
+1. Keep drilling
+2. Enter the drill");
+		
+		let mut line2 = String::new();
+		let input2 = std::io::stdin().read_line(&mut line).unwrap();
+		if line2.trim() == "1" {
+		    return "";
+		}
+		
+		if line2.trim() == "2" {
+		    return "";
+		}
+	    }
+	    
+	    else if scenarios[rng.gen_range(0..scenarios.len())] == "ancient_ruins"{
+		
+		println!("You tear open a small cave where lies a man, clearly wounded.
+1. Leave him be and keep drilling
+2. Try to save him");
+		
+		let mut line2 = String::new();
+		let input2 = std::io::stdin().read_line(&mut line).unwrap();
+		if line2.trim() == "1" {
+		    return "";
+		}
+		
+		if line2.trim() == "2" {
+		    return "";
+		}
+	    }
+	    
+	    else if scenarios[rng.gen_range(0..scenarios.len())] == "ancient_remains"{
+		
+		println!("You stumble upon the remains of a giant insect. It seems prehistoric.
+1. Keep drilling
+2. Search the remains");
+		
+		let mut line2 = String::new();
+		let input2 = std::io::stdin().read_line(&mut line).unwrap();
+		if line2.trim() == "1" {
+		    return "";
+		}
+		
+		if line2.trim() == "2" {
+		    return "";
+		}
+	    }
 	}
 	if map.content[map.ship_y as usize][map.ship_x as usize] == 0 {
-	    println!("{}", empty_texts[0]);
+	    let mut rng = rand::thread_rng();
+	    
+	    println!("{}", empty_texts[rng.gen_range(0..empty_texts.len())]);
 	}
 	map.content[map.ship_y as usize][map.ship_x as usize] = 1;
 	return "";
